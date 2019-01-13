a09098418907bc3239ac488e5758ae217eafee65
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-13 00:22:29 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+
+    attr_accessor :base_word
+
+    def initialize(base_word)
+        @base_word = base_word
+    end
+
+    def match(word_list)
+        word_list.map do |word|
+            word.split("").sort == @base_word.split("").sort ? word : nil
+        end - [nil]
+    end
+end
